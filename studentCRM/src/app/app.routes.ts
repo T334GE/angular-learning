74df185791438dd9c63548afa8fd5f188ae7523b
T334GE <teeageing@gmail.com>
2025-08-27 11:07:44 +0200

    students component

--- studentCRM/src/app/app.routes.ts
+++ studentCRM/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login-component/login-component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { Students } from './dashboard/students/students';
 
 export const routes: Routes = [
-    {path: "",component: LoginComponent},
-    {path: "dashboard",component: DashboardComponent}
+    { path: "", component: LoginComponent },
+    {
+        path: "dashboard", component: DashboardComponent, children: [
+            { path: "students", component: Students }
+        ]
+    }
 ];
--- studentCRM/src/app/dashboard/dashboard.component.ts
+++ studentCRM/src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Navigation } from './navigation/navigation';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard.component',
-  imports: [Navigation],
+  imports: [Navigation, RouterOutlet],
+  standalone: true,
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })

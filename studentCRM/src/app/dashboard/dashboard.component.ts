10d30de1b63b124707ac08117a828cacf51885df
T334GE <teeageing@gmail.com>
2025-08-27 10:01:22 +0200

    login component

---
+++ studentCRM/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Navigation } from './navigation/navigation';
+
+@Component({
+  selector: 'app-dashboard.component',
+  imports: [Navigation],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+
+}
--- studentCRM/src/app/login-component/login-component.ts
+++ studentCRM/src/app/login-component/login-component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-component',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login-component.html',
   styleUrl: './login-component.css'
 })
 export class LoginComponent {
+  loginFormular!: FormGroup
+  formBuilder = inject(FormBuilder)
+  
 
+  ngOnInit(){
+    this.buildLoginFormular()
+  }
+
+  buildLoginFormular(){
+    return this.loginFormular = this.formBuilder.nonNullable.group({
+      username:["", Validators.required],
+      password:["", Validators.required]
+    })
+  }
+
+  onSubmit(){
+    const userdata = this.loginFormular.getRawValue()
+    console.log(userdata)
+  }
 }
